38f6e9f5c1c451804b0768c09b260d7cb66bfec9
codersemon <codersemon@gmail.com>
2025-02-12 12:05:44 +0600

    data types & array learning done

---
+++ src/learn-array.ts
@@ -0,0 +1,11 @@
+// method 1: using subscript
+const ages: number[] = [2, 3, 5];
+
+// method 2: using generic style
+const numbers: Array<number> = [4, 5, 6];
+
+// multi dimensional array 
+const nestedArr: number[][] = [[1, 2, 4], [5, 6,7]]
+
+// array with different types of data 
+const rand: (string | number)[] = [3, "hello"]
---
+++ src/learn-types.ts
@@ -0,0 +1,8 @@
+let roll: number = 10;
+let username: string = "Emon";
+let isMarried: boolean = true;
+
+let random: any = "game";
+
+let multiple: number | string = 30;
+multiple = "hello";
