f29676752ce23de5147d78c732897a358acca876
codersemon <codersemon@gmail.com>
2025-02-12 19:00:05 +0600

    function & optional parameter learning done

---
+++ src/learn-function.ts
@@ -0,0 +1,11 @@
+// function getSum(a: number, b: number, c?: number): number {
+//   if (c) return a + b + c;
+//   return a + b;
+// }
+
+// console.log(getSum(2, 7));
+
+const getSumWithArrow = (a: number, b: number): number => {
+  return a + b;
+};
+console.log(getSumWithArrow(6, 7));
